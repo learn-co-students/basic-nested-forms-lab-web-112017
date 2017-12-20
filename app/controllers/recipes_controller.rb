f945239ee61093ffeda4c224b22d5f68050cc300
nspp99b <chromepanthers@gmail.com>
2017-12-20 14:33:52 -0500

    configured view, controller action, and view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    # byebug
+    @recipe = Recipe.create(recipe_params)
+    @recipe.ingredients_attributes=recipe_params[:ingredients_attributes]
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
 end
